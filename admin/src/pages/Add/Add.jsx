cd439392ca03ca45ff353dfd446c1d39df2b4e07
Page0526 <phuongtrang2652004@gmail.com>
2024-05-04 15:40:15 +0700

    fix Add

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -64,7 +64,7 @@ const Add=({url}) => {
                 <div className="add-category-price">
                     <div className="add-category flex-col">
                         <p>Product category</p>
-                        <select name='category'>
+                        <select name='category' value={data.category} onChange={onChangeHandler}>
                             <option value="Salad">Salad</option>
                             <option value="Rolls">Rolls</option>
                             <option value="Deserts">Deserts</option>
